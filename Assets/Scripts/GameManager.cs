b1c8d780d12e7dd78a98d8ecb146e3b8fa8785c7
Arthur Gandriann <arthur.ganfer@gmail.com>
2022-07-19 15:39:06 -0300

    Added Score Counter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,13 @@ public class GameManager : MonoBehaviour
 {
     private bool isGameActive = false;
     public int lifes;
+    public int score;
     private float spawnRate = 1.5f;
     private SpawnManager spawnManager;
 
     public TextMeshProUGUI gameTitle;
     public TextMeshProUGUI gameOver;
+    public TextMeshProUGUI scoreText;
     public int difficulty = 0;//0 = easy; 1 = medium; 2 = hard
 
     // Start is called before the first frame update
@@ -29,10 +31,17 @@ public class GameManager : MonoBehaviour
 
     public void StartGame(int difficulty)
     {
+        AddScore(0);
         this.difficulty = difficulty;
         SetGameStatus(true);
         StartCoroutine(SpawnTargets());
         gameTitle.gameObject.SetActive(false);
+    }
+
+    public void AddScore(int score)
+    {
+        this.score += score;
+        scoreText.text = "Score: " + this.score;
     }
 
     public void GameOver()
